8eb61b46736995f6de0b70393988fd2ed83396c4
Sahaab-tariq12 <sahaabtariq@fusionwave.co>
2025-02-18 06:16:13 +0600

    tailwind error resolve

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sidebar = () => {
+  return (
+    <div>
+      {/* logo */}
+      <div className="">
+        <h1 className=' underline text-5xl text-red-900'>heyyyy</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
